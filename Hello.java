f9274cc4e9e4eb06e5f68c4b5766feb56e6e1f03
khj <khjin1231@gmail.com>
2018-01-30 11:50:58 +0900

    added output "Tell Her World"

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ public class HelloGit{
 	public static void main(String[] args){
 		System.out.println("Hello Git");
 		System.out.println("Tell your World");
+		System.out.println("Tell Her World");
 	}
+
 }
 
